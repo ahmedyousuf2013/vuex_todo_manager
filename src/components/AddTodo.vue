d74b3da4c812db2d30f5eb1b89c3ba529b876b69
ahmed-abdallah <ahmed-abdallah@ADS101-AHQ>
2021-03-18 16:57:59 +0200

    first commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h3>Add todo</h3>
+    <div class="add">
+      <form @submit="onSbmit">
+        <input
+          type="text"
+          name=""
+          id=""
+          v-model="title"
+          placeholder="Add Todo..."
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    ...mapActions(["AddTodo"]),
+    onSbmit(e) {
+      e.preventDefault();
+      this.AddTodo(this.title);
+    },
+  },
+};
+</script>
+<style  scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 10px;
+  border: 1px solid #41b883;
+  outline: 0;
+}
+input[type="submit"] {
+  flex: 2;
+  background: #41b883;
+  color: #fff;
+  border: 1px #41b883 solid;
+  cursor: pointer;
+}
+</style>
+
+
+
+
+
---
+++ src/store/modules/todos.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+const state = {
+
+    todos: [{ id: 1, title: 'TOdo One' }, { id: 2, title: 'TOdo Two' }]
+
+};
+const getters = { allTodos: (state) => state.todos };
+const actions = {
+
+    async fetchTodos({ commit }) {
+
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+
+        commit('setTodos', response.data);
+        console.log(response.data);
+    },
+
+    async AddTodo({
+        commit
+    }, title){
+
+        const response=await axios.post('https://jsonplaceholder.typicode.com/todos',{title, completed:false});
+        commit('newTodo', response.data)
+    }
+
+};
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos ),
+    newTodo:(state,todo)=>state.todos.unshift(todo)
+
+};
+
+export default {
+
+    state,
+    getters,
+    actions,
+    mutations
+}
